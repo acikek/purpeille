fe5e7ec262e4f7a63f1b5e264c62d00bf483e0a8
acikek <spiralixp@gmail.com>
2022-04-03 17:15:56 -0700

    adjust aspect modifiers

--- src/main/java/com/acikek/purpeille/warpath/Aspect.java
+++ src/main/java/com/acikek/purpeille/warpath/Aspect.java
@@ -7,11 +7,11 @@ public enum Aspect {
     VIRTUOUS("virtuous", Tone.STRENGTH, 0, 1.4),
     EXCESS("excess", Tone.STRENGTH, 1, 1.35),
     HEROIC("heroic", Tone.STRENGTH, 2, 1.5),
-    TERRAN("terran", Tone.TENSION, 0, 1.2),
+    TERRAN("terran", Tone.TENSION, 0, 1.25),
     SHOCKING("shocking", Tone.TENSION, 1, 1.5),
     DEATHLY("deathly", Tone.TENSION, 2, 1.2),
     LIMITLESS("limitless", Tone.RELEASE, 0, 1.3),
-    TRANQUIL("tranquil", Tone.RELEASE, 1, 1.2),
+    TRANQUIL("tranquil", Tone.RELEASE, 1, 1.15),
     UNRIVALED("unrivaled", Tone.RELEASE, 2, 1.4);
 
     public String name;
