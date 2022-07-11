6d2df7f0844ccdb14010903201b0e7e7afb65cd1
acikek <spiralixp@gmail.com>
2022-07-11 19:23:36 -0400

    fix top placement when removing

--- src/main/java/com/acikek/purpeille/mixin/BlockItemMixin.java
+++ src/main/java/com/acikek/purpeille/mixin/BlockItemMixin.java
@@ -1,6 +1,7 @@
 package com.acikek.purpeille.mixin;
 
 import com.acikek.purpeille.block.entity.monolithic.MonolithicPurpur;
+import com.acikek.purpeille.block.entity.monolithic.MonolithicPurpurBlockEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemPlacementContext;
@@ -16,7 +17,8 @@ public class BlockItemMixin {
     @Inject(method = "place(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
     private void protectMonolithicPurpur(ItemPlacementContext context, CallbackInfoReturnable<ActionResult> cir) {
         BlockState state = context.getWorld().getBlockState(context.getBlockPos().down());
-        if (state.getBlock() instanceof MonolithicPurpur && state.get(MonolithicPurpur.FULL)) {
+        if (context.getWorld().getBlockEntity(context.getBlockPos().down()) instanceof MonolithicPurpurBlockEntity blockEntity
+                && (state.get(MonolithicPurpur.FULL) || blockEntity.removalTicks != 0)) {
             cir.setReturnValue(ActionResult.FAIL);
         }
     }
